ee66c03c14cd1761c901c02234a2ea82bfb8d184
Six <x@hexagram.vip>
2018-07-05 15:07:10 +0800

    1.导出公司板块数据文件block.csv； 2.导出以名称标示的公司核心图连接边信息。

--- temp.py
+++ temp.py
@@ -6,12 +6,40 @@ import re
 import time
 
 
-graph = Con_Neo4j(http='http://127.0.0.1:7474', username='neo4j', password='123456')
+# graph = Con_Neo4j(http='http://127.0.0.1:7474', username='neo4j', password='123456')
 
 
 if __name__ == '__main__':
-    with open('Data/company.csv', mode='r', encoding='utf-8', newline='') as csvfile, open('Data/company1.csv', mode='w', encoding='utf-8', newline='') as csvfile2:
-        rows = csv.reader(csvfile, delimiter=';')
-        writer = csv.writer(csvfile2, delimiter='\t')
-        for row in rows:
-            writer.writerow(row)
+    files = ['AStack_com_block', 'AStack_com_industry', 'AStack_com_invest', 'AStack_com_output']
+    with open('Data/Encoding/com_name_edge.csv', mode='w', encoding='utf-8', newline='') as file1:
+        writer = csv.writer(file1, delimiter='\t')
+        for csv_name in files:
+            csv_path = 'Data/' + csv_name + '.csv'
+            with open(csv_path, mode='r', encoding='utf-8', newline='') as file2:
+                rows = csv.reader(file2, delimiter='\t')
+                if csv_name == 'AStack_com_block':
+                    for i, row in enumerate(rows):
+                        if i == 0:
+                            continue
+                        writer_row = [row[2], 'BLOCK_'+row[3]]
+                        writer.writerow(writer_row)
+
+                if csv_name == 'AStack_com_industry':
+                    for i, row in enumerate(rows):
+                        if i == 0:
+                            continue
+                        writer_row = [row[2], 'INDUSTRY_'+row[4]]
+                        writer.writerow(writer_row)
+
+                if csv_name == 'AStack_com_invest':
+                    for i, row in enumerate(rows):
+                        if i == 0:
+                            continue
+                        writer_row = [row[2], row[6]]
+                        writer.writerow(writer_row)
+                if csv_name == 'AStack_com_output':
+                    for i, row in enumerate(rows):
+                        if i == 0:
+                            continue
+                        writer_row = [row[2], row[6]]
+                        writer.writerow(writer_row)
